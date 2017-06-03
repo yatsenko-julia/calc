1939de864fa53b216d3485691050c99499bc1bad
yatsenko-julia <yatsenko.julia@gmail.com>
2017-06-03 22:02:26 +0300

    First working version of RPN calculator

---
+++ calc.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+  float first, second;
+  char operation;
+
+  cout << "First number:  ";
+  cin >> first;
+  cout << "Second number: ";
+  cin >> second;
+  cout << "Operation: ";
+  cin >> operation;
+
+  float result;
+
+  if(operation == '+'){
+    result = first + second;
+  } 
+  else if(operation == '-'){
+    result = first - second;
+  }
+  else if(operation == '*'){
+    result = first * second;
+  }
+  else if(operation == '^'){
+    result = pow(first, second);
+  }
+  else if(second == 0 && operation == '/'){
+    cout << "na zero nizia" << endl;
+    exit(0);
+  }
+  else if(operation == '/'){
+    result = first / second;
+  } else 
+  {
+    cout << "Wrong input.";
+    exit(0);
+  }
+
+  cout <<"Result: " << result << endl;
+  return 0;
+}
