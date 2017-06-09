7576fc5e31b26b0c6b45c73ce561cc8baedb2288
yatsenko-julia <yatsenko.julia@gmail.com>
2017-06-09 12:37:37 +0300

    ne robe rpn

--- calc.cpp
+++ calc.cpp
@@ -70,6 +70,7 @@ int main(int argc, char *argv[]){
             result = first / second;
             break;
         }
+        st.push(result);
       }
      }else if(s == "="){
        cout << result << endl;
