3d40d024cc1e28fc1289eb0c75dda794f4e239d9
yatsenko-julia <yatsenko.julia@gmail.com>
2017-06-09 12:25:22 +0300

    RPN for 1 operation

--- calc.cpp
+++ calc.cpp
@@ -6,26 +6,86 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <stack>
 #include <cmath>
+#include <sstream> //make use of a class called istringstream
 using namespace std;
 
 
 int main(int argc, char *argv[]){
+  stack <float> st;
+  string s;
+  float num;
   float first, second;
   string operation;
   float result;
+
+
 
   ifstream file(argv[1]); 
 
   if(!file.is_open()){
     cerr << "File failed to open"<< endl;
-    cout << "Try enter first number:  ";
+    cout << "Input: " << endl;;
+  
+    while (true){
+      cin >> s;
+
+      if(istringstream(s) >> num){
+        st.push(num);
+       
+      } else if(s == "-" ||s == "+" ||s == "/" ||s == "*" || s == "^"){
+        second = st.top();
+        st.pop();
+        first = st.top();
+        st.pop();
+
+        switch(s.front()){
+          case '+':
+          {
+            result = first + second;
+            break;
+          } 
+          case '-':
+          {
+            result = first - second;
+            break;
+          }
+          case '*':
+          {
+            result = first * second;
+            break;
+          }
+          case '^':
+          {
+            result = pow(first, second);
+            break;
+          }
+          case '/':
+          {
+            if(second == 0){
+              cout << "na zero nizia" << endl;
+              exit(0);
+            }
+            result = first / second;
+            break;
+        }
+      }
+     }else if(s == "="){
+       cout << result << endl;
+       exit(0);
+     }  
+    }
+    
+    
+
+    /*cout << "Try enter first number:  ";
     cin >> first;
     cout << "Enter second number:  ";
     cin >> second;
     cout << "Enter operation: ";
-    cin >> operation;
-  } else{
+    cin >> operation;*/
+  } /*else{
     string enter, one, two;
     
     getline(file,enter, ' ');
@@ -76,6 +136,6 @@ int main(int argc, char *argv[]){
         exit(0);
       }
     }
-    cout <<"Result: " << result << endl;
+    cout <<"Result: " << result << endl;*/
     return 0;
 }
